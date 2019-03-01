f42b340083099f2b572fbebc89830a85d593ab24
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-03-01 12:01:50 -0600

    finished classes.js - working on stretch

--- prototypes.js
+++ prototypes.js
@@ -47,5 +47,5 @@ const cuboid = new CuboidMaker({
 });
 
 // Test your volume and surfaceArea methods by un-commenting the logs below:
- console.log(cuboid.volume()); // 100
- console.log(cuboid.surfaceArea()); // 130
+// console.log(cuboid.volume()); // 100
+// console.log(cuboid.surfaceArea()); // 130
